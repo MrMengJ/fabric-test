c328361783313d1a0f9776b12c1826ac5bb1188b
mengjin <mengjin@jecn.com.cn>
2020-08-27 16:03:01 +0800

    add initial EText and SText

---
+++ src/examples/FabricText/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef } from 'react';
+import { fabric } from 'fabric';
+import styled from 'styled-components';
+import EText from '../../objects/EText';
+import SText from '../../objects/SText';
+
+const StyledCanvas = styled.canvas`
+  width: 1000px;
+  height: 600px;
+  border: 1px solid red;
+`;
+
+let canvas;
+
+function FabricText() {
+  const canvasEl = useRef(null);
+
+  useEffect(() => {
+    if (canvasEl.current) {
+      const canvasOptions = {
+        preserveObjectStacking: true,
+        selection: true,
+        defaultCursor: 'default',
+        backgroundColor: '#f3f3f3',
+        width: 1000,
+        height: 700,
+      };
+      canvas = new fabric.Canvas(canvasEl.current, canvasOptions);
+
+      // const rect = new fabric.Rect({
+      //   width: 200,
+      //   height: 200,
+      //   fill: '#34eeeb',
+      // });
+      // canvas.add(rect);
+
+      const textBox = new SText('Test text', {
+        fontSize: 40,
+        backgroundColor: 'red',
+        fill: 'green',
+        top: 300,
+        left: 400,
+        lockMovementX: true,
+        lockMovementY: true,
+      });
+      canvas.add(textBox);
+
+      // const group = new fabric.Group([rect, textBox], {
+      //   top: 150,
+      //   left: 200,
+      // });
+      //
+      // canvas.add(group);
+
+      canvas.renderAll();
+    }
+  }, []);
+
+  return <StyledCanvas ref={canvasEl} id={'canvas'} />;
+}
+
+export default FabricText;
