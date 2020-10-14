9f8ca84e90fabb20dc8fe43a38ac4d37189ad25a
Supertis <tianshuai@jecn.com.cn>
2020-10-14 16:35:54 +0800

    [tianshuai] #N/A Redraw the DateStore element

--- src/shapes/DataStore.js
+++ src/shapes/DataStore.js
@@ -26,15 +26,20 @@ export const DataStore = fabric.util.createClass(Text, {
       h = this.height,
       x = -this.width / 2,
       y = -this.height / 2;
-
-    ctx.beginPath();
     ctx.moveTo(x, y + h / 3 / 2);
-
+    ctx.bezierCurveTo(x + w * 0.1, y + (h / 6 + h / 5), x + w * 0.9, y + (h / 6 + h / 5), x + w, y + h / 3 / 2);
+    ctx.moveTo(x, y + h / 3 / 2);
     ctx.lineTo(x, y + (h - h / 5));
     ctx.bezierCurveTo(x + w * 0.1, y + h, x + w * 0.9, y + h, x + w, y + (h - h / 5));
-
     ctx.lineTo(x + w, y + h / 3 / 2);
-    ctx.ellipse(x + w / 2, y + h / 3 / 2, w / 2, h / 3 / 2,0,0,2*Math.PI);
+    ctx.bezierCurveTo(
+        x + w * 0.9,
+        y + (h / 6 - h / 5),
+        x + w * 0.1,
+        y + (h / 6 - h / 5),
+        x,
+        y + h / 3 / 2
+    );
 
     this._renderPaintInOrder(ctx);
     this.callSuper('_render', ctx);
