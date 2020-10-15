d870ca90965deddb113eafc984474294e1f73426
Supertis <tianshuai@jecn.com.cn>
2020-10-15 18:51:16 +0800

    [tianshuai] #N/A Redraw the Activity element

--- src/shapes/Activity.js
+++ src/shapes/Activity.js
@@ -20,7 +20,29 @@ export const Activity = fabric.util.createClass(IconText, {
   direction: DIRECTION.BOTTOM,
   startColor: '#71afff',
   endColor: '#bddaff',
-  type:'Activity',
+  type: 'Activity',
+  _strokeEdge: function (ctx) {
+    let rx = this.rx ? Math.min(this.rx, this.width / 2) : 0,
+      ry = this.ry ? Math.min(this.ry, this.height / 2) : 0,
+      w = this.width,
+      h = this.height,
+      x = -this.width / 2,
+      y = -this.height / 2,
+      k = 1 - 0.5522847498,
+      isRounded = rx !== 0 || ry !== 0;
+
+    ctx.beginPath();
+    ctx.moveTo(x + rx, y);
+    ctx.lineTo(x + w - rx, y);
+    isRounded && ctx.bezierCurveTo(x + w - k * rx, y, x + w, y + k * ry, x + w, y + ry);
+    ctx.lineTo(x + w, y + h - ry);
+    isRounded &&
+      ctx.bezierCurveTo(x + w, y + h - k * ry, x + w - k * rx, y + h, x + w - rx, y + h);
+    ctx.lineTo(x + rx, y + h);
+    isRounded && ctx.bezierCurveTo(x + k * rx, y + h, x, y + h - k * ry, x, y + h - ry);
+    ctx.lineTo(x, y + ry);
+    isRounded && ctx.bezierCurveTo(x, y + k * ry, x + k * rx, y, x + rx, y);
+  },
   _render: function (ctx) {
     let rx = this.rx ? Math.min(this.rx, this.width / 2) : 0,
       ry = this.ry ? Math.min(this.ry, this.height / 2) : 0,
